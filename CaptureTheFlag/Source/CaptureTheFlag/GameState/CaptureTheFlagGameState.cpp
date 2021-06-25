3d8143989b7d1a874a0a4979ab612bf278136533
BigBertman <brysonbertuzzi@hotmail.com>
2021-06-25 11:44:07 -0400

    Added Comments to CaptureTheFlagGameState .h and .cpp

--- CaptureTheFlag/Source/CaptureTheFlag/GameState/CaptureTheFlagGameState.cpp
+++ CaptureTheFlag/Source/CaptureTheFlag/GameState/CaptureTheFlagGameState.cpp
@@ -1,9 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "CaptureTheFlagGameState.h"
 #include "Net/UnrealNetwork.h"
-
 
 void ACaptureTheFlagGameState::GetLifetimeReplicatedProps(TArray< FLifetimeProperty >& OutLifetimeProps) const
 {
--- CaptureTheFlag/Source/CaptureTheFlag/GameState/CaptureTheFlagGameState.h
+++ CaptureTheFlag/Source/CaptureTheFlag/GameState/CaptureTheFlagGameState.h
@@ -15,33 +15,43 @@ class CAPTURETHEFLAG_API ACaptureTheFlagGameState : public AGameStateBase
     GENERATED_BODY()
 
 public:
+    /** Team One Materials Template */
     UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = Materials, Replicated)
         TArray<class UMaterialInterface*> TeamOnePMaterials;
 
+    /** Team Two Materials Template */
     UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = Materials, Replicated)
         TArray<class UMaterialInterface*> TeamTwoPMaterials;
 
+    /** Team Enabled */
     UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = Gameplay, Replicated)
         bool TeamsEnabled;
 
+    /** Team One Score */
     UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = Gameplay, meta = (ClampMin = 0), Replicated)
         int TeamOneScore;
 
+    /** Team Two Score */
     UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = Gameplay, meta = (ClampMin = 0), Replicated)
         int TeamTwoScore;
 
+    /** Team One Match Score */
     UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = Gameplay, meta = (ClampMin = 0), Replicated)
         int TeamOneMatchScore;
 
+    /** Team Two Match Score */
     UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = Gameplay, meta = (ClampMin = 0), Replicated)
         int TeamTwoMatchScore;
 
+    /** Team One Size */
     UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = Gameplay, meta = (ClampMin = 0), Replicated)
         int TeamOneSize = 0;
 
+    /** Team Two Size */
     UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = Gameplay, meta = (ClampMin = 0), Replicated)
         int TeamTwoSize = 0;
 
+    /** Game Time */
     UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = Gameplay, meta = (ClampMin = 0), Replicated)
         float GameTime;
 };
